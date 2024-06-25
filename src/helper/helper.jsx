b8cef64fdfc0e3ec9e2f451c84e296c1c8c3030a
faisalach <faisalachramsyah@gmail.com>
2024-06-25 11:39:15 +0700

    update api

--- src/helper/helper.jsx
+++ src/helper/helper.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom"; */
 export const Helper = () => {
     const baseURLAPI = (url = '') => {
         url     = url.replace(/^[/]/g,'');
-        const baseURL   = 'https://flask-stunting-production.up.railway.app/api';
+        const baseURL   = 'https://flask-stunting-production.up.railway.app/api/';
         // const baseURL   = 'http://127.0.0.1:5000/api/';
         return baseURL + url;
     }
